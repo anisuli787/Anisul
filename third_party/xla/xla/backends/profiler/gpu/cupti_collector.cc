openxla/xla

Integrate PM Sampling into Xplane/Trace Viewer
PiperOrigin-RevId: 791423818

xla/backends/profiler/gpu/cupti_collector.cc
@@ -30,7 +30,6 @@ limitations under the License.
 #include "absl/container/fixed_array.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
-#include "absl/hash/hash.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/strings/str_cat.h"
@@ -51,9 +50,7 @@ limitations under the License.
 #include "xla/tsl/profiler/utils/xplane_utils.h"
 #include "tsl/platform/abi.h"
 #include "tsl/platform/host_info.h"
-#include "tsl/platform/mem.h"
 #include "tsl/platform/thread_annotations.h"
-#include "tsl/platform/types.h"
 #include "tsl/profiler/protobuf/xplane.pb.h"
 
 namespace xla {
@@ -62,12 +59,9 @@ namespace profiler {
 namespace {
 
 using tensorflow::profiler::XEventMetadata;
-using tensorflow::profiler::XLine;
-using tensorflow::profiler::XPlane;
 using tensorflow::profiler::XSpace;
 using tensorflow::profiler::XStatMetadata;
 using tsl::profiler::Annotation;
-using tsl::profiler::FindMutablePlaneWithName;
 using tsl::profiler::FindOrAddMutablePlaneWithName;
 using tsl::profiler::GpuPlaneName;
 using tsl::profiler::kCuptiActivityNvtxPlaneName;
@@ -507,7 +501,8 @@ class PerDeviceCollector {
       events_types_per_line[line_id].emplace(event.type);
     }
     device_plane->ForEachLine([&](XLineBuilder line) {
-      line.SetName(
+      // If the line name is already set, we should not override it.
+      line.SetNameIfEmpty(
           GetDeviceXLineName(line.Id(), events_types_per_line[line.Id()]));
     });
     host_plane->ForEachLine([&](XLineBuilder line) {
@@ -681,7 +676,8 @@ class EventInQueue {
 
 }  // namespace
 
-void PmSamples::PopulateCounterLine(XPlaneBuilder* plane) {
+void PmSamples::PopulateCounterLine(XPlaneBuilder* plane,
+                                    uint64_t start_gpu_time_ns) {
   XLineBuilder line = plane->GetOrCreateCounterLine();
   std::vector<std::pair<XEventMetadata*, XStatMetadata*>> counter_metadata;
   counter_metadata.reserve(metrics_.size());
@@ -694,7 +690,9 @@ void PmSamples::PopulateCounterLine(XPlaneBuilder* plane) {
     for (int i = 0; i < sampler_range.metric_values.size(); ++i) {
       XEventBuilder event = line.AddEvent(
           tsl::profiler::Timespan(
-              tsl::profiler::NanoToPico(sampler_range.start_timestamp_ns), 0),
+              tsl::profiler::NanoToPico(sampler_range.start_timestamp_ns -
+                                        start_gpu_time_ns),
+              0),
           *counter_metadata[i].first);
       event.AddStatValue(*counter_metadata[i].second,
                          sampler_range.metric_values[i]);
@@ -711,6 +709,8 @@ const std::vector<std::string>& PmSamples::GetMetrics() const {
 const std::vector<SamplerRange>& PmSamples::GetSamplerRanges() const {
   return sampler_ranges_;
 }
+
+int64_t PmSamples::GetDeviceId() const { return device_id_; }
 
 void CuptiTraceCollector::OnTracerCollectedCallbackData(
     std::vector<CallbackAnnotationsAndEvents> callback_annotations_and_events,
@@ -918,6 +918,7 @@ class CuptiTraceCollectorImpl : public CuptiTraceCollector {
                         num_activity_events_, " device events.",
                         events_dropped);
   }
+  uint64_t GetStartGputimeNs() const override { return start_gpu_ns_; }
 
  private:
   size_t num_callback_events_ = 0;

xla/backends/profiler/gpu/cupti_collector.h
@@ -61,17 +61,21 @@ struct SamplerRange {
 class PmSamples {
  public:
   PmSamples(std::vector<std::string> metrics,
-            std::vector<SamplerRange> sampler_ranges)
+            std::vector<SamplerRange> sampler_ranges, int device_id)
       : metrics_(std::move(metrics)),
-        sampler_ranges_(std::move(sampler_ranges)) {}
-  void PopulateCounterLine(tsl::profiler::XPlaneBuilder* plane);
+        sampler_ranges_(std::move(sampler_ranges)),
+        device_id_(device_id) {}
+  void PopulateCounterLine(tsl::profiler::XPlaneBuilder* plane,
+                           uint64_t start_gpu_time_ns);
   size_t GetNumSamples() const;
+  int64_t GetDeviceId() const;
   const std::vector<std::string>& GetMetrics() const;
   const std::vector<SamplerRange>& GetSamplerRanges() const;
 
  private:
   std::vector<std::string> metrics_;
   std::vector<SamplerRange> sampler_ranges_;
+  int device_id_;
 };
 
 class CuptiTraceCollector {
@@ -115,6 +119,8 @@ class CuptiTraceCollector {
   void SetTracingEndTimeNs(uint64_t end_time_ns) {
     tracing_end_time_ns_ = end_time_ns;
   }
+
+  virtual uint64_t GetStartGputimeNs() const { return 0; }
 
   uint64_t GetTracingEndTimeNs() const { return tracing_end_time_ns_; }
 

xla/backends/profiler/gpu/cupti_tracer.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include <cstdint>
 #include <ios>
 #include <list>
+#include <memory>
 #include <string>
 #include <string_view>
 #include <tuple>
@@ -48,6 +49,7 @@ limitations under the License.
 #include "xla/backends/profiler/gpu/cupti_buffer_events.h"
 #include "xla/backends/profiler/gpu/cupti_collector.h"
 #include "xla/backends/profiler/gpu/cupti_interface.h"
+#include "xla/backends/profiler/gpu/cupti_pm_sampler.h"
 #include "xla/backends/profiler/gpu/cupti_pm_sampler_factory.h"
 #include "xla/backends/profiler/gpu/cupti_utils.h"
 #include "xla/tsl/platform/env.h"
@@ -55,6 +57,8 @@ limitations under the License.
 #include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/profiler/backends/cpu/annotation_stack.h"
 #include "xla/tsl/profiler/utils/per_thread.h"
+#include "xla/tsl/profiler/utils/xplane_builder.h"
+#include "xla/tsl/profiler/utils/xplane_schema.h"
 #include "tsl/platform/host_info.h"
 #include "tsl/platform/thread_annotations.h"
 
@@ -65,6 +69,8 @@ namespace {
 
 using tsl::Env;
 using tsl::profiler::AnnotationStack;
+using tsl::profiler::GpuPlaneName;
+using tsl::profiler::XPlaneBuilder;
 
 static thread_local int internalCuCall = 0;
 
@@ -1072,8 +1078,9 @@ int CuptiTracer::NumGpus() {
   return num_gpus;
 }
 
-absl::Status CuptiTracer::Enable(const CuptiTracerOptions& option,
-                                 CuptiTraceCollector* collector) {
+absl::Status CuptiTracer::Enable(
+    const CuptiTracerOptions& option, CuptiTraceCollector* collector,
+    const std::vector<std::unique_ptr<tensorflow::profiler::XPlane>>& xplanes) {
   option_ = option;
   collector_ = collector;
 
@@ -1102,12 +1109,36 @@ absl::Status CuptiTracer::Enable(const CuptiTracerOptions& option,
   EnableActivityTracing().IgnoreError();
   tsl::profiler::AnnotationStack::Enable(true);
 
+  int num_gpus_requested = xplanes.size();
+  // Enable PM Sampling after CUPTI is initialized.
   if (option_->pm_sampler_options.enable) {
+    // Callback to populate PM sampling data to XPlane for each device.
+    option_->pm_sampler_options.process_samples =
+        [&xplanes, this](xla::profiler::PmSamples* samples) {
+          if (!samples) {
+            return;
+          }
+          int device_id = samples->GetDeviceId();
+          if (device_id < 0 || device_id >= xplanes.size()) {
+            LOG(ERROR) << "Device ID " << device_id << " out of range.";
+            return;
+          }
+          if (!xplanes[device_id]) {
+            LOG(ERROR) << "Device ID " << device_id << " XPlane is null.";
+            return;
+          }
+          tensorflow::profiler::XPlane* xplane = xplanes[device_id].get();
+          xplane->set_name(GpuPlaneName(device_id));
+          XPlaneBuilder xplane_builder(xplane);
+          samples->PopulateCounterLine(&xplane_builder,
+                                       collector_->GetStartGputimeNs());
+        };
+    // Creates PM sampler object.
     TF_ASSIGN_OR_RETURN(
-        pm_sampler_, CreatePmSampler(NumGpus(), option_->pm_sampler_options));
+        cupti_pm_sampler_,
+        CreatePmSampler(num_gpus_requested, option_->pm_sampler_options));
 
-    TF_RETURN_IF_ERROR(pm_sampler_->StartSampler());
-
+    TF_RETURN_IF_ERROR(cupti_pm_sampler_->StartSampler());
     pm_sampling_enabled_ = true;
   }
 
@@ -1115,9 +1146,14 @@ absl::Status CuptiTracer::Enable(const CuptiTracerOptions& option,
 }
 
 void CuptiTracer::Disable() {
+  // Disables the PM Sampling before Cupti Tracer is disabled.
   if (pm_sampling_enabled_) {
-    pm_sampler_->StopSampler().IgnoreError();
-    pm_sampler_->Deinitialize().IgnoreError();
+    if (absl::Status status = cupti_pm_sampler_->StopSampler(); !status.ok()) {
+      LOG(WARNING) << "Failed to stop PM sampler: " << status;
+    }
+    if (absl::Status status = cupti_pm_sampler_->Deinitialize(); !status.ok()) {
+      LOG(WARNING) << "Failed to deinitialize PM sampler: " << status;
+    }
 
     pm_sampling_enabled_ = false;
   }

anisuli787/Anisul

third_party/xla/xla/backends/profiler/gpu/cupti_collector.cc
@@ -30,7 +30,6 @@ limitations under the License.
 #include "absl/container/fixed_array.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
-#include "absl/hash/hash.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/strings/str_cat.h"
@@ -51,9 +50,7 @@ limitations under the License.
 #include "xla/tsl/profiler/utils/xplane_utils.h"
 #include "tsl/platform/abi.h"
 #include "tsl/platform/host_info.h"
-#include "tsl/platform/mem.h"
 #include "tsl/platform/thread_annotations.h"
-#include "tsl/platform/types.h"
 #include "tsl/profiler/protobuf/xplane.pb.h"
 
 namespace xla {
@@ -62,12 +59,9 @@ namespace profiler {
 namespace {
 
 using tensorflow::profiler::XEventMetadata;
-using tensorflow::profiler::XLine;
-using tensorflow::profiler::XPlane;
 using tensorflow::profiler::XSpace;
 using tensorflow::profiler::XStatMetadata;
 using tsl::profiler::Annotation;
-using tsl::profiler::FindMutablePlaneWithName;
 using tsl::profiler::FindOrAddMutablePlaneWithName;
 using tsl::profiler::GpuPlaneName;
 using tsl::profiler::kCuptiActivityNvtxPlaneName;
@@ -507,7 +501,8 @@ class PerDeviceCollector {
       events_types_per_line[line_id].emplace(event.type);
     }
     device_plane->ForEachLine([&](XLineBuilder line) {
-      line.SetName(
+      // If the line name is already set, we should not override it.
+      line.SetNameIfEmpty(
           GetDeviceXLineName(line.Id(), events_types_per_line[line.Id()]));
     });
     host_plane->ForEachLine([&](XLineBuilder line) {
@@ -681,7 +676,8 @@ class EventInQueue {
 
 }  // namespace
 
-void PmSamples::PopulateCounterLine(XPlaneBuilder* plane) {
+void PmSamples::PopulateCounterLine(XPlaneBuilder* plane,
+                                    uint64_t start_gpu_time_ns) {
   XLineBuilder line = plane->GetOrCreateCounterLine();
   std::vector<std::pair<XEventMetadata*, XStatMetadata*>> counter_metadata;
   counter_metadata.reserve(metrics_.size());
@@ -694,7 +690,9 @@ void PmSamples::PopulateCounterLine(XPlaneBuilder* plane) {
     for (int i = 0; i < sampler_range.metric_values.size(); ++i) {
       XEventBuilder event = line.AddEvent(
           tsl::profiler::Timespan(
-              tsl::profiler::NanoToPico(sampler_range.start_timestamp_ns), 0),
+              tsl::profiler::NanoToPico(sampler_range.start_timestamp_ns -
+                                        start_gpu_time_ns),
+              0),
           *counter_metadata[i].first);
       event.AddStatValue(*counter_metadata[i].second,
                          sampler_range.metric_values[i]);
@@ -711,6 +709,8 @@ const std::vector<std::string>& PmSamples::GetMetrics() const {
 const std::vector<SamplerRange>& PmSamples::GetSamplerRanges() const {
   return sampler_ranges_;
 }
+
+int64_t PmSamples::GetDeviceId() const { return device_id_; }
 
 void CuptiTraceCollector::OnTracerCollectedCallbackData(
     std::vector<CallbackAnnotationsAndEvents> callback_annotations_and_events,
@@ -918,6 +918,7 @@ class CuptiTraceCollectorImpl : public CuptiTraceCollector {
                         num_activity_events_, " device events.",
                         events_dropped);
   }
+  uint64_t GetProfileStartTimeNs() const override { return start_gpu_ns_; }
 
  private:
   size_t num_callback_events_ = 0;

third_party/xla/xla/backends/profiler/gpu/cupti_collector.h
@@ -61,17 +61,21 @@ struct SamplerRange {
 class PmSamples {
  public:
   PmSamples(std::vector<std::string> metrics,
-            std::vector<SamplerRange> sampler_ranges)
+            std::vector<SamplerRange> sampler_ranges, int device_id)
       : metrics_(std::move(metrics)),
-        sampler_ranges_(std::move(sampler_ranges)) {}
-  void PopulateCounterLine(tsl::profiler::XPlaneBuilder* plane);
+        sampler_ranges_(std::move(sampler_ranges)),
+        device_id_(device_id) {}
+  void PopulateCounterLine(tsl::profiler::XPlaneBuilder* plane,
+                           uint64_t start_gpu_time_ns);
   size_t GetNumSamples() const;
+  int64_t GetDeviceId() const;
   const std::vector<std::string>& GetMetrics() const;
   const std::vector<SamplerRange>& GetSamplerRanges() const;
 
  private:
   std::vector<std::string> metrics_;
   std::vector<SamplerRange> sampler_ranges_;
+  int device_id_;
 };
 
 class CuptiTraceCollector {
@@ -115,6 +119,8 @@ class CuptiTraceCollector {
   void SetTracingEndTimeNs(uint64_t end_time_ns) {
     tracing_end_time_ns_ = end_time_ns;
   }
+
+  virtual uint64_t GetProfileStartTimeNs() const { return 0; }
 
   uint64_t GetTracingEndTimeNs() const { return tracing_end_time_ns_; }
 

third_party/xla/xla/backends/profiler/gpu/cupti_tracer.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include <cstdint>
 #include <ios>
 #include <list>
+#include <memory>
 #include <string>
 #include <string_view>
 #include <tuple>
@@ -48,6 +49,7 @@ limitations under the License.
 #include "xla/backends/profiler/gpu/cupti_buffer_events.h"
 #include "xla/backends/profiler/gpu/cupti_collector.h"
 #include "xla/backends/profiler/gpu/cupti_interface.h"
+#include "xla/backends/profiler/gpu/cupti_pm_sampler.h"
 #include "xla/backends/profiler/gpu/cupti_pm_sampler_factory.h"
 #include "xla/backends/profiler/gpu/cupti_utils.h"
 #include "xla/tsl/platform/env.h"
@@ -55,6 +57,8 @@ limitations under the License.
 #include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/profiler/backends/cpu/annotation_stack.h"
 #include "xla/tsl/profiler/utils/per_thread.h"
+#include "xla/tsl/profiler/utils/xplane_builder.h"
+#include "xla/tsl/profiler/utils/xplane_schema.h"
 #include "tsl/platform/host_info.h"
 #include "tsl/platform/thread_annotations.h"
 
@@ -65,6 +69,8 @@ namespace {
 
 using tsl::Env;
 using tsl::profiler::AnnotationStack;
+using tsl::profiler::GpuPlaneName;
+using tsl::profiler::XPlaneBuilder;
 
 static thread_local int internalCuCall = 0;
 
@@ -1072,8 +1078,9 @@ int CuptiTracer::NumGpus() {
   return num_gpus;
 }
 
-absl::Status CuptiTracer::Enable(const CuptiTracerOptions& option,
-                                 CuptiTraceCollector* collector) {
+absl::Status CuptiTracer::Enable(
+    const CuptiTracerOptions& option, CuptiTraceCollector* collector,
+    const std::vector<std::unique_ptr<tensorflow::profiler::XPlane>>& xplanes) {
   option_ = option;
   collector_ = collector;
 
@@ -1102,12 +1109,36 @@ absl::Status CuptiTracer::Enable(const CuptiTracerOptions& option,
   EnableActivityTracing().IgnoreError();
   tsl::profiler::AnnotationStack::Enable(true);
 
+  int num_gpus_requested = xplanes.size();
+  // Enable PM Sampling after CUPTI is initialized.
   if (option_->pm_sampler_options.enable) {
+    // Callback to populate PM sampling data to XPlane for each device.
+    option_->pm_sampler_options.process_samples =
+        [&xplanes, this](xla::profiler::PmSamples* samples) {
+          if (!samples) {
+            return;
+          }
+          int device_id = samples->GetDeviceId();
+          if (device_id < 0 || device_id >= xplanes.size()) {
+            LOG(ERROR) << "Device ID " << device_id << " out of range.";
+            return;
+          }
+          if (!xplanes[device_id]) {
+            LOG(ERROR) << "Device ID " << device_id << " XPlane is null.";
+            return;
+          }
+          tensorflow::profiler::XPlane* xplane = xplanes[device_id].get();
+          xplane->set_name(GpuPlaneName(device_id));
+          XPlaneBuilder xplane_builder(xplane);
+          samples->PopulateCounterLine(&xplane_builder,
+                                       collector_->GetProfileStartTimeNs());
+        };
+    // Creates PM sampler object.
     TF_ASSIGN_OR_RETURN(
-        pm_sampler_, CreatePmSampler(NumGpus(), option_->pm_sampler_options));
+        cupti_pm_sampler_,
+        CreatePmSampler(num_gpus_requested, option_->pm_sampler_options));
 
-    TF_RETURN_IF_ERROR(pm_sampler_->StartSampler());
-
+    TF_RETURN_IF_ERROR(cupti_pm_sampler_->StartSampler());
     pm_sampling_enabled_ = true;
   }
 
@@ -1115,9 +1146,14 @@ absl::Status CuptiTracer::Enable(const CuptiTracerOptions& option,
 }
 
 void CuptiTracer::Disable() {
+  // Disables the PM Sampling before Cupti Tracer is disabled.
   if (pm_sampling_enabled_) {
-    pm_sampler_->StopSampler().IgnoreError();
-    pm_sampler_->Deinitialize().IgnoreError();
+    if (absl::Status status = cupti_pm_sampler_->StopSampler(); !status.ok()) {
+      LOG(WARNING) << "Failed to stop PM sampler: " << status;
+    }
+    if (absl::Status status = cupti_pm_sampler_->Deinitialize(); !status.ok()) {
+      LOG(WARNING) << "Failed to deinitialize PM sampler: " << status;
+    }
 
     pm_sampling_enabled_ = false;
   }